fdbf49d8a5e9afd9f4a3452bac33704d5057f888
João Brandão <jpab1998@hotmail.com>
2019-05-15 09:43:34 +0100

    init commit
    Signed-off-by: João Brandão <jpab1998@hotmail.com>

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1557909554,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tile_wall.png",
+		"images/player-sheet0.png",
+		"images/obj_pill-sheet0.png",
+		"images/obj_player2-sheet0.png",
+		"images/obj_block-sheet0.png",
+		"images/finish-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
